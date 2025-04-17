6350f69a4676717bfefe7d8d562993bf8223d5e5
Minhdev2004 <chinhtpc123@gmail.com>
2025-04-17 17:51:25 +0700

    update

--- Study/my-react-app/src/App.js
+++ Study/my-react-app/src/App.js
@@ -1,11 +1,78 @@
+import { useState, useEffect } from 'react';
 
-import './App.css';
-
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <h1>Welcome to Tic Tac Toe!</h1>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
-export default App;
+function calculateWinter(squares)
+{
+  const lines =[[0,1,2],[3,4,5],[6,7,8], [0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  for(let i=0;i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+}
+export default function Board() {
+  const [xIsNext,setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //[null, null, null, null, null, null, null, null, null]
+  const winner = calculateWinter(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner;
+  }
+  else{
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i) {
+    if(squares[i] || calculateWinter(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();  // Create a copy of the squares array
+    if(xIsNext) {
+      nextSquares[i]="X";
+    }
+    else {
+      nextSquares[i]="O";
+    }
+    setXIsNext(!xIsNext);
+    // Check if the square is already filled
+    setSquares(nextSquares);
+  }
+  /* useEffect(
+    ()=> {
+      const lines =[[0,1,2],[3,4,5],[6,7,8], [0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+      alert(lines[0][1]);
+
+  }
+) */
+  return (
+    <>
+    
+    <div className="status">{status}</div>
+    <div className="Board-main">
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+      </div>
+    </>
+  );
+}
